b0f18bf8af027e7e0c45bf4622a637b3cd4bbb20
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2023-12-27 13:05:05 +0100

    linting

--- src/entities/exercise.entity.ts
+++ src/entities/exercise.entity.ts
@@ -1,17 +1,16 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Exercise {
+	@PrimaryGeneratedColumn("uuid")
+	id: string;
 
-  @PrimaryGeneratedColumn("uuid")
-  id: string;
+	@Column("varchar")
+	name: string;
 
-  @Column("varchar")
-  name: string;
+	@Column("varchar", { nullable: true })
+	description?: string;
 
-  @Column("varchar", { nullable: true })
-  description?: string;
-
-  @Column("simple-array")
-  videos: string[];
+	@Column("simple-array")
+	videos: string[];
 }
