37d106da1c45f8d9d9a0f88834c71dc8fcbe86a3
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2024-04-05 21:17:36 +0200

    improved logging

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -4,7 +4,22 @@ export function logger(req: Request, res: Response, next: NextFunction): void {
 	const isDocs = req.path.startsWith("/docs");
 
 	if (!isDocs) {
-		console.log(`${req.method.toUpperCase()}: ${req.path}`);
+		const method = req.method;
+		const path = req.path;
+		const dateNowString = new Date().toISOString();
+
+		const logMessage = buildLogMessage(method, path, dateNowString);
+
+		console.log(logMessage);
 	}
 	next();
+}
+
+function paddedLeft(str: string, length = 7): string {
+	const pad = " ".repeat(length - str.length);
+	return `${pad}${str}`;
+}
+
+function buildLogMessage(method: string, path: string, dateNowString: string): string {
+	return `${dateNowString} ${paddedLeft(method)} ${path}`;
 }
