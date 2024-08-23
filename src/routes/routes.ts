977eea4a50821d0aa99dcf073b372ee0830a088a
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2024-08-23 23:33:28 +0200

    register user routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,7 @@ import passport from "passport";
 import authRoutes from "./auth.routes";
 import exerciseRoutes from "./exercise.routes";
 import setsRoutes from "./sets.routes";
+import userRoutes from "./user.routes";
 import workoutsRoutes from "./workouts.routes";
 
 export function registerRoutes(app: Application) {
@@ -11,4 +12,5 @@ export function registerRoutes(app: Application) {
 	app.use("/exercises", passport.authenticate("jwt", { session: false }), exerciseRoutes);
 	app.use("/workouts", passport.authenticate("jwt", { session: false }), workoutsRoutes);
 	app.use("/sets", passport.authenticate("jwt", { session: false }), setsRoutes);
+	app.use("/user", passport.authenticate("jwt", { session: false }), userRoutes);
 }
